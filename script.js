4847eff99b8506adf5eda88e5a7cc27d694d09de
Shubhrajit Santra <shubhrajit.santra@gmail.com>
2024-06-23 21:45:53 +0530

    Main page with background image, navigation bar, title and logo

---
+++ script.js
@@ -0,0 +1,5 @@
+document.body.style.backgroundSize = `${screen.width}px ${screen.height}px`;
+document.body.style.width = `${screen.width}px`;
+// document.body.style.height = `${screen.height}px`;
+console.log(screen.width);
+console.log(screen.height);
